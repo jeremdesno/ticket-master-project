976042adc54f947b7b5ea5eed86dbaa113aacec5
jeremdesno <jjdesnoyer@gmail.com>
2024-09-28 13:48:38 +0200

    add(settings): add formatting rules with prettier

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'import'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
@@ -17,9 +17,22 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
+    // TypeScript rules
     '@typescript-eslint/explicit-function-return-type': 'error',
     '@typescript-eslint/explicit-module-boundary-types': 'error',
     '@typescript-eslint/no-explicit-any': 'error',
+
+    // Prettier integration
+    'prettier/prettier': 'error',
+
+    // Import order rule
+    'import/order': [
+      'error',
+      {
+        'groups': [['builtin', 'external', 'internal']],
+        'newlines-between': 'always',  
+        'alphabetize': { order: 'asc', caseInsensitive: true }, 
+      },
+    ],
   },
 };
