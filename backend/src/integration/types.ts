2db3a086db3b356d18648e75d9156679766606dd
jeremdesno <jjdesnoyer@gmail.com>
2024-09-28 19:51:08 +0200

    feat(integration): init ticketmaster api responses types

---
+++ backend/src/integration/types.ts
@@ -0,0 +1,48 @@
+/**
+ * @description This file contains TypeScript interfaces used for
+ * interacting with the Ticketmaster API.
+ */
+
+export interface Link {
+  href: string;
+  templated?: boolean;
+}
+
+export interface Links {
+  self: Link;
+  next?: Link;
+  prev?: Link;
+}
+
+export interface Event {
+  id: string;
+  name: string;
+  dates: object;
+  url: string;
+  description: string;
+  type: string;
+  sales: object;
+  place: object;
+  [key: string]:
+    | string
+    | number
+    | Array<string | number | object | boolean>
+    | object;
+}
+
+export interface Embedded {
+  events: Event[];
+}
+
+export interface Page {
+  size: number;
+  totalElements: number;
+  totalPages: number;
+  number: number;
+}
+
+export interface EventsResponse {
+  _links: Links;
+  _embedded: Embedded;
+  page: Page;
+}
