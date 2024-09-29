2798b1eefd984943be07f9de0274bd9e3ad1df92
jeremdesno <jjdesnoyer@gmail.com>
2024-09-29 19:30:20 +0200

    feat(database): init folder and event data model

---
+++ backend/src/database/events.ts
@@ -0,0 +1,13 @@
+export interface EventDataModel {
+  id: string;
+  name: string;
+  startDate: string;
+  endDate: string;
+  url: string;
+  description: string;
+  eventType: string;
+  startDateSales: string;
+  endDateSales: string;
+  venueAddress: string;
+  venueName: string;
+}
