05e1d7522c60bc65067066ce474ca4a0fa7139c7
jeremdesno <jjdesnoyer@gmail.com>
2024-10-12 21:49:01 +0200

    feat(home page): integrate home page container in the main app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,14 @@
 import './App.css';
 import React from 'react';
 
+import HomePageContainer from './containers/HomePageContainer';
 import NavBarContainer from './containers/NavBarContainer';
 
 function App(): JSX.Element {
   return (
     <div className="App">
       <NavBarContainer />
-      Building....
+      <HomePageContainer />
     </div>
   );
 }
