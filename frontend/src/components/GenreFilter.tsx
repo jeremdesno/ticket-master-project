9b955ac8e2fa8230d49f74836ae036a3840f9e10
jeremdesno <jjdesnoyer@gmail.com>
2024-10-13 22:26:04 +0200

    feat(genre page): init genre dropdown menu

---
+++ frontend/src/components/GenreFilter.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import styles from '../styles/Filters.module.css';
+
+interface GenreFilterProps {
+  handleDropdowntoggle: () => void;
+  isDropdownOpen: boolean;
+  genres: string[];
+  selectedGenre: string;
+  onGenreSelect: (genre: string) => void;
+}
+
+const GenreFilter: React.FC<GenreFilterProps> = ({
+  handleDropdowntoggle,
+  isDropdownOpen,
+  genres,
+  selectedGenre,
+  onGenreSelect,
+}) => {
+  return (
+    <div className={styles.genreFilter}>
+      <button
+        onClick={handleDropdowntoggle}
+        className={styles.genreDropdownButton}
+      >
+        {selectedGenre || 'Select Genre'}
+        <span className={styles.genreDropdownArrow}>
+          {isDropdownOpen ? '▲' : '▼'}
+        </span>
+      </button>
+      {isDropdownOpen && (
+        <div className={styles.genreDropdownMenu}>
+          {genres.map((genre) => (
+            <button
+              key={genre}
+              onClick={(): void => onGenreSelect(genre)}
+              className={`${styles.genreItem} ${
+                selectedGenre === genre ? styles.genreSelected : ''
+              }`}
+            >
+              {genre}
+            </button>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default GenreFilter;
