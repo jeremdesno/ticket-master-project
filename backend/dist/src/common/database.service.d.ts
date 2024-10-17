a26c52780c92da5386deeaf08c0efa09ec8e5903
jeremdesno <jjdesnoyer@gmail.com>
2024-10-17 12:26:08 +0200

    Stop tracking backend/dist folder

--- backend/dist/src/common/database.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { ConfigService } from '@nestjs/config';
-import { Kysely } from 'kysely';
-import { DatabaseSchema } from './models';
-export declare class DatabaseService {
-    private configService;
-    private readonly database;
-    constructor(configService: ConfigService);
-    getDatabase(): Kysely<DatabaseSchema>;
-}
