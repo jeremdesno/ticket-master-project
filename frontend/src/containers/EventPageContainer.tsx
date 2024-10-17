8932f6fcd2b123b0982e2e5a5994587e98f723b3
jeremdesno <jjdesnoyer@gmail.com>
2024-10-17 12:22:42 +0200

    feat(event page): fetch event from api and handle case when no event is returned

--- frontend/src/containers/EventPageContainer.tsx
+++ frontend/src/containers/EventPageContainer.tsx
@@ -1,25 +1,10 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 
 import { EventDataModel } from '../../../backend/src/common/models';
+import { fetchEvent } from '../api/eventService';
 import EventCard from '../components/EventCard';
 import styles from '../styles/EventPage.module.css';
-
-/* To be fetched from the API using the id */
-const event: EventDataModel = {
-  id: '1',
-  name: 'Jazz Night Concert',
-  startDate: new Date('2024-12-20'),
-  endDate: new Date('2024-12-21'),
-  url: 'https://example.com/jazz-night',
-  description:
-    'Join us for an unforgettable Jazz Night Concert featuring world-class musicians. Enjoy a night of smooth melodies and vibrant performances.',
-  genre: 'Jazz',
-  startDateSales: new Date('2024-11-01'),
-  endDateSales: new Date('2024-12-19'),
-  venueAddress: '123 Music Avenue, New York, NY',
-  venueName: 'Jazz Hall',
-};
 
 const relatedEvents: EventDataModel[] = Array.from(
   { length: 4 },
@@ -39,77 +24,98 @@ const relatedEvents: EventDataModel[] = Array.from(
 );
 
 const EventPage: React.FC = (): React.JSX.Element => {
+  const { eventId } = useParams<{
+    eventId: string;
+  }>() as { eventId: string };
   const imageUrl = ''; // Image URL is empty for the fake event
-
+  const [event, setEvent] = useState<EventDataModel | null>(null);
+  useEffect(() => {
+    const loadEvent = async (): Promise<void> => {
+      try {
+        const fetchedEvent = await fetchEvent(eventId);
+        setEvent(fetchedEvent);
+      } catch (error) {
+        console.error('Failed to fetch event:', error);
+      }
+    };
+    loadEvent();
+  }, [event]);
+  console.log(event);
   const navigate = useNavigate();
   const handleDetailsClick = (eventId: string): void => {
     navigate(`/event/${eventId}`);
   };
-
-  return (
-    <div className={styles.eventPageContainer}>
-      <div className={styles.eventDetailsSection}>
-        <div className={styles.leftSection}>
-          {imageUrl ? (
-            <img
-              src={imageUrl}
-              alt={event.name}
-              className={styles.eventImage}
-            />
-          ) : (
-            <div className={styles.imageUnavailable}>Image Unavailable</div>
-          )}
-          <div className={styles.eventDetails}>
-            <p>
-              <strong>Venue:</strong> {event.venueName}
-            </p>
-            <p>
-              <strong>Address:</strong> {event.venueAddress}
-            </p>
-            <p>
-              <strong>Start Date:</strong>{' '}
-              {event.startDate.toLocaleDateString()}
-            </p>
-            <p>
-                <strong>End Date:</strong> {event.endDate ? event.endDate.toLocaleDateString() : 'Not defined'}
-            </p>
-            <p>
-              <strong>Sales Period:</strong>{' '}
-              {event.startDateSales.toLocaleDateString()} -{' '}
-              {event.endDateSales.toLocaleDateString()}
-            </p>
+  if (event == null) {
+    return <div>An error happened while fetching the event...</div>;
+  } else {
+    return (
+      <div className={styles.eventPageContainer}>
+        <div className={styles.eventDetailsSection}>
+          <div className={styles.leftSection}>
+            {imageUrl ? (
+              <img
+                src={imageUrl}
+                alt={event.name}
+                className={styles.eventImage}
+              />
+            ) : (
+              <div className={styles.imageUnavailable}>Image Unavailable</div>
+            )}
+            <div className={styles.eventDetails}>
+              <p>
+                <strong>Venue:</strong> {event.venueName}
+              </p>
+              <p>
+                <strong>Address:</strong> {event.venueAddress}
+              </p>
+              <p>
+                <strong>Start Date:</strong>{' '}
+                {event.startDate.toLocaleDateString()}
+              </p>
+              <p>
+                <strong>End Date:</strong>{' '}
+                {event.endDate
+                  ? event.endDate.toLocaleDateString()
+                  : 'Not defined'}
+              </p>
+              <p>
+                <strong>Sales Period:</strong>{' '}
+                {event.startDateSales.toLocaleDateString()} -{' '}
+                {event.endDateSales.toLocaleDateString()}
+              </p>
+            </div>
+            <a
+              className={styles.accessWebsiteButton}
+              href={event.url}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Event Website
+            </a>
           </div>
-          <a
-            className={styles.accessWebsiteButton}
-            href={event.url}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Event Website
-          </a>
-        </div>
 
-        <div className={styles.rightSection}>
-          <h1>{event.name}</h1>
-          <p>{event.description}</p>
+          <div className={styles.rightSection}>
+            <h1>{event.name}</h1>
+            <p>{event.description}</p>
+          </div>
+        </div>
+        <div className={styles.suggestedEventsSection}>
+          <h3>You Might Like</h3>
+          <div className={styles.suggestedEventsContainer}>
+            {relatedEvents.map((event) => (
+              <EventCard
+                key={event.id}
+                event={event}
+                onDetailsClick={(): void => {
+                  handleDetailsClick(event.id);
+                }}
+              />
+            ))}
+          </div>
         </div>
       </div>
-      <div className={styles.suggestedEventsSection}>
-        <h3>You Might Like</h3>
-        <div className={styles.suggestedEventsContainer}>
-          {relatedEvents.map((event) => (
-            <EventCard
-              key={event.id}
-              event={event}
-              onDetailsClick={(): void => {
-                handleDetailsClick(event.id);
-              }}
-            />
-          ))}
-        </div>
-      </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default EventPage;
