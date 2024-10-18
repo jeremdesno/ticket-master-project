134dbb6f2c0cbb6dd962433e0f3082aecebc3270
jeremdesno <jjdesnoyer@gmail.com>
2024-10-18 14:54:12 +0200

    refacto(integration): moove processing and parsing functions to utils file

--- backend/src/integration/integration.service.ts
+++ backend/src/integration/integration.service.ts
@@ -2,14 +2,9 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import axios from 'axios';
 import { Kysely } from 'kysely';
-import sharp from 'sharp';
 
-import {
-  ClassificationsResponse,
-  EventExtracted,
-  EventsResponse,
-} from './types';
-import { findClosestImage, targetHeight, targetWidth } from './utils';
+import { ClassificationsResponse, EventsResponse } from './types';
+import { parsePageClassifications, parsePageEvents } from './utils';
 import { DatabaseService } from '../common/database.service';
 import {
   DatabaseSchema,
@@ -22,7 +17,6 @@ import {
 export class IntegrationService {
   private readonly baseUrl = 'https://app.ticketmaster.com';
   private readonly apiKey: string;
-  private readonly imgBBApiKey: string;
   private readonly database: Kysely<DatabaseSchema>;
 
   constructor(
@@ -30,50 +24,7 @@ export class IntegrationService {
     private databaseService: DatabaseService,
   ) {
     this.apiKey = this.configService.get<string>('TICKETMASTER_API_KEY');
-    this.imgBBApiKey = this.configService.get<string>('IMGBB_API_KEY');
     this.database = this.databaseService.getDatabase();
-  }
-
-  async resizeImage(imageUrl: string): Promise<Buffer> {
-    try {
-      const response = await axios.get<ArrayBuffer>(imageUrl, {
-        responseType: 'arraybuffer',
-      });
-      const imageBuffer = Buffer.from(response.data);
-
-      return await sharp(imageBuffer)
-        .resize(targetWidth, targetHeight)
-        .toBuffer();
-    } catch (error) {
-      console.error('Error fetching or processing image: ', error);
-    }
-  }
-
-  async uploadToImgBB(imageBuffer: Buffer): Promise<string> {
-    const base64Image = imageBuffer.toString('base64');
-    const imgBBUrl = `https://api.imgbb.com/1/upload`;
-
-    try {
-      const formData = {
-        key: this.imgBBApiKey,
-        image: base64Image,
-        expiration: 172800, // 2 jours
-      };
-
-      const response = await axios.post(imgBBUrl, formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
-      });
-
-      if (response.data && response.data.data.url) {
-        return response.data.data.url;
-      } else {
-        throw new Error('Failed to upload image to imgBB');
-      }
-    } catch (error) {
-      console.error('Error uploading image to imgBB: ', error);
-    }
   }
 
   async getEvents(): Promise<EventsResponse> {
@@ -103,83 +54,6 @@ export class IntegrationService {
     }
   }
 
-  async parsePageEvents(data: EventsResponse): Promise<EventDataModel[]> {
-    if (!data._embedded || !data._embedded.events) {
-      throw new Error('Invalid data structure');
-    }
-
-    const events = await Promise.all(
-      data._embedded.events.map(async (event: EventExtracted) => {
-        const venue = event._embedded?.venues?.[0];
-        const venueAddress = [
-          venue.address?.line1,
-          venue.address?.line2,
-          venue.address?.line3,
-        ]
-          .filter(Boolean)
-          .join(', ');
-
-        const startDate = event.dates.start.dateTBD
-          ? null
-          : new Date(
-              `${event.dates.start.localDate}T${event.dates.start.localTime || '00:00:00'}`,
-            );
-
-        const endDate = event.dates.end?.dateTBD
-          ? null
-          : event.dates.end?.localDate
-            ? new Date(
-                `${event.dates.end.localDate}T${event.dates.end.localTime || '23:59:59'}`,
-              )
-            : null;
-
-        const startDateSales = event.sales.public.startDateTime
-          ? new Date(event.sales.public.startDateTime)
-          : null;
-
-        const endDateSales = event.sales.public.endDateTime
-          ? new Date(event.sales.public.endDateTime)
-          : null;
-
-        const genre: string[] = [];
-        for (const classification of event.classifications) {
-          genre.push(
-            classification.segment?.name === 'Undefined'
-              ? 'Others'
-              : classification.segment?.name,
-          );
-        }
-
-        const closestImage = findClosestImage(event.images);
-        let imageUrl: string | null = null;
-        if (closestImage) {
-          try {
-            const resizedImageBuffer = await this.resizeImage(closestImage.url);
-            imageUrl = await this.uploadToImgBB(resizedImageBuffer);
-          } catch (error) {
-            console.error('Error processing image: ', error);
-          }
-        }
-
-        return {
-          id: event.id,
-          name: event.name,
-          startDate: startDate,
-          endDate: endDate,
-          startDateSales: startDateSales,
-          endDateSales: endDateSales,
-          url: event.url,
-          description: event.description,
-          genre: genre.join('/'),
-          venueAddress: venueAddress || 'No address available',
-          venueName: venue.name || 'No information',
-          imageUrl: imageUrl,
-        };
-      }),
-    );
-    return events;
-  }
-
   async upsertEvent(event: EventDataModel): Promise<void> {
     await this.database
       .insertInto('events')
@@ -206,7 +80,7 @@ export class IntegrationService {
     let nextUrl: string | null = data._links.self.href;
 
     while (nextUrl) {
-      const pageEventsParsed = await this.parsePageEvents(data);
+      const pageEventsParsed = await parsePageEvents(data);
 
       for (const parsedEvent of pageEventsParsed) {
         this.upsertEvent(parsedEvent);
@@ -241,37 +115,6 @@ export class IntegrationService {
     }
   }
 
-  async parsePageClassifications(
-    data: ClassificationsResponse,
-  ): Promise<[GenreDataModel[], SubGenreDataModel[]]> {
-    if (!data._embedded || !data._embedded.classifications) {
-      throw new Error('Invalid data structure');
-    }
-    const subGenreList: SubGenreDataModel[] = [];
-    const genreList: GenreDataModel[] = [];
-    data._embedded.classifications.forEach((classification) => {
-      const genreId = classification.segment?.id;
-      const genreName =
-        classification.segment?.name === 'Undefined'
-          ? 'Others'
-          : classification.segment?.name;
-      if (genreId && genreName) {
-        genreList.push({ id: genreId, name: genreName });
-      }
-
-      if (classification?.segment?._embedded?.genres) {
-        classification.segment._embedded.genres.forEach((subGenre) => {
-          subGenreList.push({
-            id: subGenre.id,
-            name: subGenre.name,
-            genreId: genreId,
-          });
-        });
-      }
-    });
-    return [genreList, subGenreList];
-  }
-
   async upsertGenre(genre: GenreDataModel): Promise<void> {
     await this.database
       .insertInto('genres')
@@ -304,7 +147,7 @@ export class IntegrationService {
 
     while (nextUrl) {
       const [pageGenresParsed, pageSubGenresParsed] =
-        await this.parsePageClassifications(data);
+        await parsePageClassifications(data);
 
       for (const parsedGenre of pageGenresParsed) {
         this.upsertGenre(parsedGenre);
--- backend/src/integration/utils.ts
+++ backend/src/integration/utils.ts
@@ -1,4 +1,17 @@
-import { Image } from './types';
+import axios from 'axios';
+import sharp from 'sharp';
+import {
+  EventDataModel,
+  GenreDataModel,
+  SubGenreDataModel,
+} from 'src/common/models';
+
+import {
+  ClassificationsResponse,
+  EventExtracted,
+  EventsResponse,
+  Image,
+} from './types';
 
 export const targetHeight = 200;
 export const targetWidth = 300;
@@ -20,3 +33,156 @@ export const findClosestImage = (images: Image[]): Image | null => {
 
   return closestImage;
 };
+
+export async function resizeImage(imageUrl: string): Promise<Buffer> {
+  try {
+    const response = await axios.get<ArrayBuffer>(imageUrl, {
+      responseType: 'arraybuffer',
+    });
+    const imageBuffer = Buffer.from(response.data);
+
+    return await sharp(imageBuffer)
+      .resize(targetWidth, targetHeight)
+      .toBuffer();
+  } catch (error) {
+    console.error('Error fetching or processing image: ', error);
+  }
+}
+
+export async function uploadToImgBB(imageBuffer: Buffer): Promise<string> {
+  const imgBBApiKey = process.env.IMGBB_API_KEY;
+  const base64Image = imageBuffer.toString('base64');
+  const imgBBUrl = `https://api.imgbb.com/1/upload`;
+
+  try {
+    const formData = {
+      key: imgBBApiKey,
+      image: base64Image,
+      expiration: 172800, // 2 jours
+    };
+
+    const response = await axios.post(imgBBUrl, formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    });
+
+    if (response.data && response.data.data.url) {
+      return response.data.data.url;
+    } else {
+      throw new Error('Failed to upload image to imgBB');
+    }
+  } catch (error) {
+    console.error('Error uploading image to imgBB: ', error);
+  }
+}
+
+export async function parsePageEvents(
+  data: EventsResponse,
+): Promise<EventDataModel[]> {
+  if (!data._embedded || !data._embedded.events) {
+    throw new Error('Invalid data structure');
+  }
+
+  const events = await Promise.all(
+    data._embedded.events.map(async (event: EventExtracted) => {
+      const venue = event._embedded?.venues?.[0];
+      const venueAddress = [
+        venue.address?.line1,
+        venue.address?.line2,
+        venue.address?.line3,
+      ]
+        .filter(Boolean)
+        .join(', ');
+
+      const startDate = event.dates.start.dateTBD
+        ? null
+        : new Date(
+            `${event.dates.start.localDate}T${event.dates.start.localTime || '00:00:00'}`,
+          );
+
+      const endDate = event.dates.end?.dateTBD
+        ? null
+        : event.dates.end?.localDate
+          ? new Date(
+              `${event.dates.end.localDate}T${event.dates.end.localTime || '23:59:59'}`,
+            )
+          : null;
+
+      const startDateSales = event.sales.public.startDateTime
+        ? new Date(event.sales.public.startDateTime)
+        : null;
+
+      const endDateSales = event.sales.public.endDateTime
+        ? new Date(event.sales.public.endDateTime)
+        : null;
+
+      const genre: string[] = [];
+      for (const classification of event.classifications) {
+        genre.push(
+          classification.segment?.name === 'Undefined'
+            ? 'Others'
+            : classification.segment?.name,
+        );
+      }
+
+      const closestImage = findClosestImage(event.images);
+      let imageUrl: string | null = null;
+      if (closestImage) {
+        try {
+          const resizedImageBuffer = await resizeImage(closestImage.url);
+          imageUrl = await uploadToImgBB(resizedImageBuffer);
+        } catch (error) {
+          console.error('Error processing image: ', error);
+        }
+      }
+
+      return {
+        id: event.id,
+        name: event.name,
+        startDate: startDate,
+        endDate: endDate,
+        startDateSales: startDateSales,
+        endDateSales: endDateSales,
+        url: event.url,
+        description: event.description,
+        genre: genre.join('/'),
+        venueAddress: venueAddress || 'No address available',
+        venueName: venue.name || 'No information',
+        imageUrl: imageUrl,
+      };
+    }),
+  );
+  return events;
+}
+
+export async function parsePageClassifications(
+  data: ClassificationsResponse,
+): Promise<[GenreDataModel[], SubGenreDataModel[]]> {
+  if (!data._embedded || !data._embedded.classifications) {
+    throw new Error('Invalid data structure');
+  }
+  const subGenreList: SubGenreDataModel[] = [];
+  const genreList: GenreDataModel[] = [];
+  data._embedded.classifications.forEach((classification) => {
+    const genreId = classification.segment?.id;
+    const genreName =
+      classification.segment?.name === 'Undefined'
+        ? 'Others'
+        : classification.segment?.name;
+    if (genreId && genreName) {
+      genreList.push({ id: genreId, name: genreName });
+    }
+
+    if (classification?.segment?._embedded?.genres) {
+      classification.segment._embedded.genres.forEach((subGenre) => {
+        subGenreList.push({
+          id: subGenre.id,
+          name: subGenre.name,
+          genreId: genreId,
+        });
+      });
+    }
+  });
+  return [genreList, subGenreList];
+}
