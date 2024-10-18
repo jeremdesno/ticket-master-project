ddb2e5e4967bdde19e24644bcd1f1540f28bd20f
jeremdesno <jjdesnoyer@gmail.com>
2024-10-18 17:47:22 +0200

    feat(events integration): created runnable script to sync events and sessions with the extracted events data

---
+++ backend/scripts/sync-events-and-sessions.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { IntegrationModule } from '../src/integration/integration.module'; 
+import { IntegrationService } from '../src/integration/integration.service';
+
+async function syncEventsAndSessions() {
+  const app = await NestFactory.createApplicationContext(IntegrationModule);
+  const integrationService = app.get(IntegrationService);
+
+  try {
+    const newExtractedEvents = await integrationService.getNewExtractedEvents();
+    console.log(`Fetched ${newExtractedEvents.length} new sessions`)
+    await integrationService.syncExtractedEventsToSessionsAndEvents(newExtractedEvents);
+    console.log('Events and Sessions synced');
+  } catch (error) {
+    console.error('Error:', error.message);
+  } finally {
+    await app.close(); 
+  }
+}
+
+syncEventsAndSessions().catch((error) => {
+  console.error('Error syncing data:', error);
+});
