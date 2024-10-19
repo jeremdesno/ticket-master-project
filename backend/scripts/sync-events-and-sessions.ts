97bef88bc62c40ea6486e81d0e90acadc15878fb
jeremdesno <jjdesnoyer@gmail.com>
2024-10-19 14:20:53 +0200

    feat(events integration): get and sync new events in runnable script

--- backend/scripts/sync-events-and-sessions.ts
+++ backend/scripts/sync-events-and-sessions.ts
@@ -9,7 +9,11 @@ async function syncEventsAndSessions() {
   try {
     const newExtractedEvents = await integrationService.getNewExtractedEvents();
     console.log(`Fetched ${newExtractedEvents.length} new sessions`)
+    const newEvents = await integrationService.getNewEvents();
+    console.log(`Fetched ${newEvents.length} new events`)
+
     await integrationService.syncSessions(newExtractedEvents);
+    await integrationService.syncEvents(newEvents)
     console.log('Events and Sessions synced');
   } catch (error) {
     console.error('Error:', error.message);
