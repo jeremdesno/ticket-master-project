70966e90e326dfa1b266fee401b2924124be705f
jeremdesno <jjdesnoyer@gmail.com>
2024-10-21 15:21:00 +0200

    feat(events integration): add indexing of events to elasticsearch node in syncing script

--- backend/scripts/sync-events-and-sessions.ts
+++ backend/scripts/sync-events-and-sessions.ts
@@ -6,19 +6,29 @@ async function syncEventsAndSessions() {
   const app = await NestFactory.createApplicationContext(IntegrationModule);
   const integrationService = app.get(IntegrationService);
 
-  try {
-    const newExtractedEvents = await integrationService.getNewExtractedEvents();
-    console.log(`Fetched ${newExtractedEvents.length} new sessions`)
-    const newEvents = await integrationService.getNewEvents();
-    console.log(`Fetched ${newEvents.length} new events`)
+  const connectionStatus = await integrationService.doHealthChecks();
+  if (connectionStatus){
+    try {
+      // Fetch new sessions and events
+      const newExtractedEvents = await integrationService.getNewExtractedEvents();
+      console.log(`Fetched ${newExtractedEvents.length} new sessions`);
 
-    await integrationService.syncSessions(newExtractedEvents);
-    await integrationService.syncEvents(newEvents)
-    console.log('Events and Sessions synced');
-  } catch (error) {
-    console.error('Error:', error.message);
-  } finally {
-    await app.close(); 
+      const newEvents = await integrationService.getNewEvents();
+      console.log(`Fetched ${newEvents.length} new events`);
+
+      await integrationService.syncSessions(newExtractedEvents);
+      await integrationService.syncEvents(newEvents);
+      await integrationService.indexEvents(newEvents)
+      
+
+      console.log('Events and Sessions synced and indexed');
+    } catch (error) {
+      console.error('Error:', error.message);
+    } finally {
+      await app.close(); 
+    }
+  } else {
+    throw new Error('Failed to connect to postgresql and elasticsearch instances')
   }
 }
 
