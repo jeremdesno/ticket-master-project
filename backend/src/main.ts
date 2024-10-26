e53499714806420733c9d9b791bf1922810d90b7
jeremdesno <jjdesnoyer@gmail.com>
2024-10-26 18:53:21 +0200

    feat(api): use cookie parser

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import cookieParser from 'cookie-parser';
 
 import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
   app.enableCors({
     origin: 'http://localhost:3001',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
