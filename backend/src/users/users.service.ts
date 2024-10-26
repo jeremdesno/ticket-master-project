7f86e83b019e2e4bc0c26cf2c916317a2355d4c4
jeremdesno <jjdesnoyer@gmail.com>
2024-10-26 11:50:16 +0200

    feat(users): create service

---
+++ backend/src/users/users.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { Kysely } from 'kysely';
+
+import { DatabaseService } from '../common/database.service';
+import { DatabaseSchema, User } from '../common/models';
+
+@Injectable()
+export class UsersService {
+  private readonly database: Kysely<DatabaseSchema>;
+
+  constructor(private readonly databaseService: DatabaseService) {
+    this.database = this.databaseService.getDatabase();
+  }
+
+  async createUser(username: string, password: string): Promise<void> {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await this.database
+      .insertInto('users')
+      .values({ username, password: hashedPassword })
+      .execute();
+  }
+
+  async findByUsername(username: string): Promise<User | undefined> {
+    const users = await this.database
+      .selectFrom('users')
+      .selectAll()
+      .where('username', '=', username)
+      .execute();
+    return users[0];
+  }
+}
