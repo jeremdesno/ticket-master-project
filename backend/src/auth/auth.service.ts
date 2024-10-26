bf49aaef6b47bd1f362eb946d2e879040b191242
jeremdesno <jjdesnoyer@gmail.com>
2024-10-26 11:59:45 +0200

    feat(auth): add auth service

---
+++ backend/src/auth/auth.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { User } from 'src/common/models';
+
+import { UsersService } from '../users/users.service';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateUser(username: string, password: string): Promise<User | null> {
+    const user = await this.usersService.findByUsername(username);
+    if (user && (await bcrypt.compare(password, user.password))) {
+      return user;
+    }
+    return null;
+  }
+
+  async login(user: User): Promise<{ access_token: string }> {
+    const payload = { username: user.username, sub: user.id };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+}
