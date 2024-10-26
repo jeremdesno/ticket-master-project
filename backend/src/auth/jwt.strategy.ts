2cf3bbec3e45c00c8d8f347af69df03867820f73
jeremdesno <jjdesnoyer@gmail.com>
2024-10-26 12:01:15 +0200

    feat(auth): add jwt strategy and jwt auth-guard

---
+++ backend/src/auth/jwt.strategy.ts
@@ -0,0 +1,25 @@
+// src/auth/jwt.strategy.ts
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { User } from 'src/common/models';
+
+import { UsersService } from '../users/users.service';
+
+interface JwtPayload {
+  username: string;
+}
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private usersService: UsersService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
+
+  async validate(payload: JwtPayload): Promise<User | undefined> {
+    return this.usersService.findByUsername(payload.username);
+  }
+}
