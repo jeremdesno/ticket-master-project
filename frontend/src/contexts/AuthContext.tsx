3e8b01652b90c035138e732c7d62ccc63f32d3c9
jeremdesno <jjdesnoyer@gmail.com>
2024-10-27 18:28:13 +0100

    feat(auth): expose callback in provider hook to update AuthContext state from another component in the app

--- frontend/src/contexts/AuthContext.tsx
+++ frontend/src/contexts/AuthContext.tsx
@@ -13,7 +13,11 @@ interface AuthStatus {
   username: string | null;
 }
 
-const AuthContext = createContext<AuthStatus | undefined>(undefined);
+interface AuthContextType extends AuthStatus {
+  checkAuth: () => Promise<void>; // Expose checkAuth in the context
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 interface AuthProviderProps {
   children: ReactNode;
@@ -21,28 +25,33 @@ interface AuthProviderProps {
 
 export const AuthProvider: React.FC<AuthProviderProps> = ({
   children,
-}): ReactNode => {
+}): JSX.Element => {
   const [auth, setAuth] = useState<AuthStatus>({
     isAuthenticated: false,
     username: null,
   });
 
+  const checkAuth = async (): Promise<void> => {
+    try {
+      const username = await whoami();
+      setAuth({ isAuthenticated: true, username });
+    } catch (error) {
+      setAuth({ isAuthenticated: false, username: null });
+    }
+  };
+
   useEffect(() => {
-    const checkAuth = async (): Promise<void> => {
-      try {
-        const username = await whoami();
-        setAuth({ isAuthenticated: true, username });
-      } catch (error) {
-        setAuth({ isAuthenticated: false, username: null });
-      }
-    };
     checkAuth();
   }, []);
 
-  return <AuthContext.Provider value={auth}>{children}</AuthContext.Provider>;
+  return (
+    <AuthContext.Provider value={{ ...auth, checkAuth }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
-export const useAuth = (): AuthStatus => {
+export const useAuth = (): AuthContextType => {
   const context = useContext(AuthContext);
   if (!context) {
     throw new Error('useAuth must be used within an AuthProvider');
