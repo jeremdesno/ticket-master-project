4e31298c84e226230ef51866444c368009c9fc47
jeremdesno <jjdesnoyer@gmail.com>
2024-10-27 15:10:40 +0100

    feat(auth): set the login and logout endpoints to public

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -8,11 +8,13 @@ import {
 import { Response } from 'express';
 
 import { AuthService } from './auth.service';
+import { Public } from './public.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Public()
   @Post('login')
   async login(
     @Body() body: { username: string; password: string },
@@ -35,6 +37,7 @@ export class AuthController {
     return { message: 'Logged in successfully' };
   }
 
+  @Public()
   @Post('logout')
   async logout(
     @Res({ passthrough: true }) response: Response,
