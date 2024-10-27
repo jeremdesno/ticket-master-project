c643780a3e936794d9bb7e0f375ae6c681451a4c
jeremdesno <jjdesnoyer@gmail.com>
2024-10-27 16:19:31 +0100

    add: missing typing in JwtStrategy and move JwtPayload interface to dedicated file

--- backend/src/auth/jwt.strategy.ts
+++ backend/src/auth/jwt.strategy.ts
@@ -1,20 +1,18 @@
 // src/auth/jwt.strategy.ts
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
+import { Request } from 'express';
 import { Strategy } from 'passport-jwt';
 import { User } from 'src/common/models';
 
+import { JwtPayload } from './types';
 import { UsersService } from '../users/users.service';
-
-interface JwtPayload {
-  username: string;
-}
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(private usersService: UsersService) {
     super({
-      jwtFromRequest: (req) => {
+      jwtFromRequest: (req: Request) => {
         return req.cookies?.jwt || null;
       },
       secretOrKey: process.env.JWT_SECRET,
