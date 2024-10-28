e11ed872a062340cec202a2c3f5e21680a8db274
jeremdesno <jjdesnoyer@gmail.com>
2024-10-28 14:58:47 +0100

    feat(NavBar): use Auth context to display login or logout button in nav bar

--- frontend/src/containers/NavBarContainer.tsx
+++ frontend/src/containers/NavBarContainer.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
 import GenreMenuContainer from './GenreMenuContainer';
+import LoginButtonContainer from './LoginButonContainer';
 import LogoutButtonContainer from './LogoutButtonContainer';
 import SearchBarContainer from './SearchBarContainer';
 import FavoritesButton from '../components/FavoritesButton';
 import HomeButton from '../components/HomeButton';
+import { useAuth } from '../contexts/AuthContext';
 import styles from '../styles/NavBar.module.css';
 
 const NavBarContainer: React.FC = () => {
+  const { isAuthenticated } = useAuth();
   return (
     <nav className={styles.navbar}>
       <div className={styles.leftSectionNavBar}>
@@ -17,7 +20,7 @@ const NavBarContainer: React.FC = () => {
       </div>
       <div className={styles.rightSectionNavBar}>
         <SearchBarContainer />
-        <LogoutButtonContainer />
+        {isAuthenticated ? <LogoutButtonContainer /> : <LoginButtonContainer />}
       </div>
     </nav>
   );
