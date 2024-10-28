89c5273ca82672c39ef5ff59657ef148ec2653a0
jeremdesno <jjdesnoyer@gmail.com>
2024-10-28 17:58:45 +0100

    feat(api requests): added axios interceptor to raise an alert and redirect user when the token has expired

--- frontend/src/api/axios.tsx
+++ frontend/src/api/axios.tsx
@@ -9,4 +9,35 @@ const axiosInstance = axios.create({
   withCredentials: true,
 });
 
+let sessionExpired = false;
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const { response } = error;
+
+    if (response) {
+      switch (response.status) {
+        case 401:
+          if (!sessionExpired) {
+            sessionExpired = true; // Prevents multiple requests raisong this alertalerts
+
+            alert('Your session has expired. Redirecting to login.');
+
+            window.location.href = '/';
+
+            setTimeout(() => {
+              sessionExpired = false;
+            }, 10000);
+          }
+          break;
+        default:
+          break;
+      }
+    }
+
+    return Promise.reject(error);
+  },
+);
+
 export default axiosInstance;
