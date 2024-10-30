45a6f28044ce6a2d04c29a491d5154c72cd15820
jeremdesno <jjdesnoyer@gmail.com>
2024-10-30 15:38:37 +0100

    feat(favorites page): implement page using the EventBoxContainer

--- frontend/src/pages/FavoritesPage.tsx
+++ frontend/src/pages/FavoritesPage.tsx
@@ -1,7 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
+import { FavoriteEventsDataModel } from '../../../backend/src/common/models';
+import { fetchFavorites, removeFavorite } from '../api/favoritesService';
+import EventBoxContainer from '../containers/EventBoxContainer';
+import { useAuth } from '../contexts/AuthContext';
+import styles from '../styles/FavoritesPage.module.css';
 const FavoritesPage: React.FC = (): React.JSX.Element => {
-  return <div>Building...</div>;
+  const { userId } = useAuth() as { userId: number };
+  const [favorites, setFavorites] = useState<FavoriteEventsDataModel[] | null>(
+    null,
+  );
+
+  useEffect(() => {
+    const loadFavorites = async (userId: number): Promise<void> => {
+      const fetchedFavorites = await fetchFavorites(userId);
+      setFavorites(fetchedFavorites);
+    };
+    loadFavorites(userId);
+  }, [userId, favorites]);
+
+  const handleRemoveFavorite = async (eventId: string): Promise<void> => {
+    await removeFavorite(userId, eventId);
+    setFavorites(
+      (prevFavorites) =>
+        prevFavorites?.filter((fav) => fav.eventId !== eventId) || null,
+    );
+  };
+
+  if (!favorites) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <div className={styles.favoritesGrid}>
+      {favorites.map((favorite) => (
+        <EventBoxContainer
+          key={favorite.eventId}
+          eventId={favorite.eventId}
+          onRemoveFavorite={(): void => {
+            handleRemoveFavorite(favorite.eventId);
+          }}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default FavoritesPage;
