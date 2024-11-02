603f727dce976b47d47366397dc339e0048d6f4e
jeremdesno <jjdesnoyer@gmail.com>
2024-11-02 12:39:23 +0100

    feat(auth): create ownership guard

--- backend/src/auth/guards/ownership.auth-guard.ts
+++ backend/src/auth/guards/ownership.auth-guard.ts
@@ -1,10 +1,10 @@
 import {
-    Injectable,
-    CanActivate,
-    ExecutionContext,
-    UnauthorizedException,
-  } from '@nestjs/common';
-  
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
+
 @Injectable()
 export class OwnershipGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean {
@@ -13,12 +13,13 @@ export class OwnershipGuard implements CanActivate {
     const userIdFromRequest = request.params.userId || request.body.userId;
 
     if (!userIdFromRequest) {
-    throw new UnauthorizedException('No userId provided in the request.');
+      throw new UnauthorizedException('No userId provided in the request.');
     }
     if (user.id !== Number(userIdFromRequest)) {
-    throw new UnauthorizedException('Access denied. You do not own this resource.');
+      throw new UnauthorizedException(
+        'Access denied. You do not own this resource.',
+      );
     }
     return true;
   }
 }
-  
