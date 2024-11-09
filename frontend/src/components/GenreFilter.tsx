0a04f051c477eb7c62ae6004802e2567ace47179
jeremdesno <jjdesnoyer@gmail.com>
2024-11-09 13:28:37 +0100

    refacto(genre filter): move styling from filter css module to a dedicated genre filter css module

--- frontend/src/components/GenreFilter.tsx
+++ frontend/src/components/GenreFilter.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import styles from '../styles/Filters.module.css';
+import styles from '../styles/components/GenreFilter.module.css';
 
 interface GenreFilterProps {
   handleDropdowntoggle: () => void;
