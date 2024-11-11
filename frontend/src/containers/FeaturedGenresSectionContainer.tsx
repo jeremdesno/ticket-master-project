381c1626d4dc4f543c627181e4f8f199b1532147
jeremdesno <jjdesnoyer@gmail.com>
2024-11-11 15:00:44 +0100

    feat(genres section): implement fetching data logic and update styling

--- frontend/src/containers/FeaturedGenresSectionContainer.tsx
+++ frontend/src/containers/FeaturedGenresSectionContainer.tsx
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
+import { GenreDataModel } from '../../../backend/src/common/models';
+import { fetchGenres } from '../api/genreService';
 import ImageSection from '../components/ImageSection';
+import { genreImagesPaths } from '../constants/genres';
 import styles from '../styles/components/FeaturedGenresSection.module.css';
 
 const FeaturedGenresSectionContainer: React.FC = (): JSX.Element => {
-  const images = [
-    'https://via.placeholder.com',
-    'https://via.placeholder.com',
-    'https://via.placeholder.com',
-    'https://via.placeholder.com',
-  ];
+  const [genres, setGenres] = useState<GenreDataModel[] | null>(null);
 
+  useEffect(() => {
+    const loadGenres = async (): Promise<void> => {
+      try {
+        const fetchedGenres = await fetchGenres();
+        setGenres(fetchedGenres);
+      } catch (error) {
+        console.log('Failed to load genres:', error);
+      }
+    };
+    loadGenres();
+  }, []);
+  if (!genres) {
+    return <div>Loading genres...</div>;
+  }
   return (
     <ImageSection
-      images={images}
+      images={genres
+        .filter((genre) => genre.id in genreImagesPaths)
+        .map((genre) => {
+          return genreImagesPaths[genre.id];
+        })}
       styles={{
         imagesLayoutContainer: styles.verticalStack,
         imageContainer: styles.genreImage,
