7e17ff071bd5e32ba7b16a21132ebd9e0a92fdf8
jeremdesno <jjdesnoyer@gmail.com>
2024-11-11 14:58:53 +0100

    add: genre images in public folder and created constant file to store image paths

---
+++ frontend/src/constants/genres.tsx
@@ -0,0 +1,6 @@
+export const genreImagesPaths: { [key: string]: string } = {
+  KZFzniwnSyZfZ7v7na: '/genres/arts_and_theatre.png',
+  KZFzniwnSyZfZ7v7nE: '/genres/sports.png',
+  KZFzniwnSyZfZ7v7nn: '/genres/film.png',
+  KZFzniwnSyZfZ7v7nJ: '/genres/music.png',
+};
